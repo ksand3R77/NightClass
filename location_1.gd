cd65acb2f2a3d810dc25de69b63a2a2c00278bdb
ksand3R77 <ivankazakov080@gmail.com>
2025-05-21 20:26:32 +0400

    сделал камеру и немного локацию 2

--- location_1.gd
+++ location_1.gd
@@ -1,9 +1,13 @@
 extends Node2D
 
 
-func _on_button_pressed() -> void:
+
+
+func _on_button_1_door_pressed() -> void:
 	get_tree().change_scene_to_file("res://location_2.tscn")
+	
+	
 
 
-func _on_button_2_pressed() -> void:
+func _on_button_2_door_pressed() -> void:
 	get_tree().change_scene_to_file("res://location_3.tscn")
