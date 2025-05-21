0d23c4a1c80c2d013c04c7827b65f29b38a36812
ksand3R77 <ivankazakov080@gmail.com>
2025-05-21 18:17:37 +0400

    добавил локации

---
+++ node_2d.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+
+func _on_button_1_door_pressed() -> void:
+	get_tree().change_scene_to_file("res://location_2.tscn")
+
+
+func _on_button_2_door_pressed() -> void:
+	get_tree().change_scene_to_file("res://location_3.tscn")
